e87ed50611cbb0a55a63a3f91fea0670d1456712
ferra71@hotmail.com <https://ibizsolutions.visualstudio.com/DefaultCollection/APL/_git/Shared.Schemas.EHM>
2017-01-24 07:51:47 +0100

    init

---
+++ Src/Component/TerminateBasedOnXPath.Component.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using BizTalkComponents.Utils;
+
+namespace Shared.PipelineComponents
+{
+    public partial class TerminateBasedOnXPath
+    {
+        public string Name { get { return "TerminateBasedOnXPath"; } }
+        public string Version { get { return "1.0"; } }
+        public string Description { get { return "Terminates message if Path cannot be found"; } }
+
+        public void GetClassID(out Guid classID)
+        {
+            classID = new Guid("E7F67FC1-FC6C-40B2-BD94-14AA7BD4F9D7");
+        }
+
+        public void InitNew()
+        {
+
+        }
+
+        public IEnumerator Validate(object projectSystem)
+        {
+            return ValidationHelper.Validate(this, false).ToArray().GetEnumerator();
+        }
+
+        public bool Validate(out string errorMessage)
+        {
+            var errors = ValidationHelper.Validate(this, true).ToArray();
+
+            if (errors.Any())
+            {
+                errorMessage = string.Join(",", errors);
+
+                return false;
+            }
+
+            errorMessage = string.Empty;
+
+            return true;
+        }
+
+        public IntPtr Icon { get { return IntPtr.Zero; } }
+    }
+}
---
+++ Src/Component/TerminateBasedOnXPath.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Xml;
+using System.Xml.XPath;
+using Microsoft.BizTalk.Component.Interop;
+using Microsoft.BizTalk.Message.Interop;
+using Microsoft.BizTalk.Streaming;
+using Microsoft.BizTalk.XPath;
+using IComponent = Microsoft.BizTalk.Component.Interop.IComponent;
+using BizTalkComponents.Utils;
+
+namespace Shared.PipelineComponents
+{
+    [ComponentCategory(CategoryTypes.CATID_PipelineComponent)]
+    [System.Runtime.InteropServices.Guid("E7F67FC1-FC6C-40B2-BD94-14AA7BD4F9D7")]
+    [ComponentCategory(CategoryTypes.CATID_Any)]
+    public partial class TerminateBasedOnXPath : IComponent, IBaseComponent, IPersistPropertyBag, IComponentUI
+    {
+       
+        private const string XPathPropertyName = "XPath";
+       
+
+        [DisplayName("XPath")]
+        [Description("The XPath to be evaluated")]
+        [RequiredRuntime]
+        public string XPath { get; set; }
+
+       
+
+        public IBaseMessage Execute(IPipelineContext pContext, IBaseMessage pInMsg)
+        {
+           
+            bool terminate = true;
+
+            IBaseMessagePart bodyPart = pInMsg.BodyPart;
+
+           
+
+            Stream inboundStream = bodyPart.GetOriginalDataStream();
+            VirtualStream virtualStream = new VirtualStream(VirtualStream.MemoryFlag.AutoOverFlowToDisk);
+            ReadOnlySeekableStream readOnlySeekableStream = new ReadOnlySeekableStream(inboundStream, virtualStream);
+
+            XmlTextReader xmlTextReader = new XmlTextReader(readOnlySeekableStream);
+            XPathCollection xPathCollection = new XPathCollection();
+            XPathReader xPathReader = new XPathReader(xmlTextReader, xPathCollection);
+            xPathCollection.Add(XPath);
+
+            while (xPathReader.ReadUntilMatch())
+            {
+                if (xPathReader.Match(0))
+                {
+                    terminate = false;
+ 
+                    break;
+                }
+            }
+
+           
+            if( terminate == true)
+            {
+                pInMsg = null;
+            }
+           
+
+            return pInMsg;
+        }
+
+        public void Load(IPropertyBag propertyBag, int errorLog)
+        {
+            XPath = PropertyBagHelper.ReadPropertyBag(propertyBag, XPathPropertyName, XPath);
+         
+        }
+
+        public void Save(IPropertyBag propertyBag, bool clearDirty, bool saveAllProperties)
+        {
+            PropertyBagHelper.WritePropertyBag(propertyBag, XPathPropertyName, XPath);
+        }
+    }
+}
